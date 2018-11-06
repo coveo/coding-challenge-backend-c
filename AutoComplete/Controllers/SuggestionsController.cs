62e91a34294b135c34474c893418aba97a6fbabd
Francois Rivard <frivard@GENETEC.COM>
2018-11-05 20:13:27 -0500

    First commit, does nothing but can answer a basic query.

---
+++ AutoComplete/Controllers/SuggestionsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoComplete.Controllers
+{
+    [Route("suggestions")]
+    [ApiController]
+    public class SuggestionsController : ControllerBase
+    {
+        // GET /suggestions
+        [HttpGet]
+        public ActionResult<SuggestionsResult> Get(string q, double? latitude, double? longitude)
+        {
+            var ret = new SuggestionsResult();
+            ret.AddItem(new SuggestionsResultItem() { name = "**YOUR QUERY** London, WITH POS,++" + q, latitude = latitude.HasValue ? latitude.ToString() : "no lat given", longitude = longitude.HasValue? longitude.ToString() : "no long given", score = 0.9 });
+            return ret;
+        }
+
+    }
+}
---
+++ AutoComplete/Controllers/SuggestionsResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoComplete.Controllers
+{
+    public class SuggestionsResultItem
+    {
+        public string name { get; set; }
+        public string latitude { get; set; }
+        public string longitude { get; set; }
+        public double score { get; set; }
+    }
+
+    public class SuggestionsResult
+    {
+        public List<SuggestionsResultItem> suggestions =  new List<SuggestionsResultItem>();
+
+        public void AddItem(SuggestionsResultItem item)
+        {
+            suggestions.Add(item);
+        }
+    }
+}
